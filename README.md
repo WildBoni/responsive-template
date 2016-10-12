f1547c70626ca2ea5d0b50b9d4ec8ed7dc24446b
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-10-12 12:42:11 +0200

    Fix: readme link for online version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Responsive template
+
+This is a simple template with fully responsive features. Thanks to its mobile-first design, it scales well on different devices, from smartphones to desktops.
+
+## Online version
+
+You can see the online version here [https://wildboni.github.io/responsive-template/index.html](https://wildboni.github.io/responsive-template/index.html)
+
+## Language used
+* HTML5
+* CSS3
+* Javascript
+
+## Features
+
+These are the main features (please note that the project is still under development):
+
+- [x] HTML5 semantic elements
+- [x] Responsive layout
+- [X] Responsive "hamburger" menu
+- [ ] Jquery animations
--- js/script.js
+++ js/script.js
@@ -6,26 +6,23 @@
   matcher.addListener(chooseLayout);
 
 // Define some functions to toggle the side panel with different media queries
-
   function panelToggleButtonMobile() {
     drawer.classList.toggle('open');
-    // panelToggleButtonMobile.stopPropagation();
   }
   function panelToggleAreaMobile() {
     drawer.classList.remove('open');
-    // panelToggleAreaMobile.stopPropagation();
   }
   function panelToggleButtonTablet() {
     drawer.classList.toggle('close');
     main.classList.toggle('close');
-    // panelToggleButtonTablet.stopPropagation();
   }
   function panelToggleAreaTablet() {
     drawer.classList.add('close');
     main.classList.add('close');
-    // panelToggleAreaTablet.stopPropagation();
   }
 
+// Depending on the device viewport, we toggle classes and listeners
+// to show or hide the side panel.
   function chooseLayout(matcher) {
     if (matcher.matches) {
       main.classList.remove('close');
